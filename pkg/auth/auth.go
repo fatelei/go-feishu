dfc103f9abf41c202b8636f80653e835b50d489c
fatelei <fatelei@gmail.com>
2020-04-15 20:39:31 +0800

    refactor: auth logic

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,11 +4,9 @@ import (
 	"encoding/json"
 	"github.com/fatelei/go-feishu/pkg/model"
 	transport2 "github.com/fatelei/go-feishu/pkg/transport"
-	"time"
 )
 
 type Auth struct {
-	accessToken *model.AccessToken
 	appID string
 	appSecret string
 	transport *transport2.Transport
@@ -18,7 +16,6 @@ type Auth struct {
 func NewAuth(appID string, appSecret string, endPoint string) *Auth {
 	transport := &transport2.Transport{Endpoint: endPoint}
 	return &Auth{
-		accessToken: nil,
 		appID:       appID,
 		appSecret:   appSecret,
 		transport:	 transport,
@@ -27,25 +24,17 @@ func NewAuth(appID string, appSecret string, endPoint string) *Auth {
 
 
 func (p *Auth) GetAccessToken() *model.AccessToken {
-	flag := false
-	if p.accessToken == nil {
-		flag = true
-	} else if time.Now().Unix() - p.accessToken.Expire <= 10 {
-		flag = true
-	}
-	if flag {
-		resp, err := p.transport.PostJson("/open-apis/auth/v3/tenant_access_token/internal", p.generateParam())
-		if err != nil {
+	resp, err := p.transport.PostJson("/open-apis/auth/v3/tenant_access_token/internal", p.generateParam())
+	if err != nil {
 			panic(err)
 		} else {
-		    tmp := &model.AccessToken{}
-			if err := json.Unmarshal(resp, tmp); err != nil {
+	    tmp := &model.AccessToken{}
+		if err := json.Unmarshal(resp, tmp); err != nil {
 				panic(err)
 			}
-			p.accessToken = tmp
-		}
+		return tmp
 	}
-	return p.accessToken
+	return nil
 }
 
 func (p *Auth) generateParam() map[string]interface{} {
